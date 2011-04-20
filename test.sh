7572e2a231ca5f2fddc685682c0335cb64e88de7
Francois Berenger <berenger@riken.jp>
2011-04-20 17:00:02 +0900

    executables had their names changed to be more speakful

--- test.sh
+++ test.sh
@@ -20,16 +20,18 @@
 #set -x
 
 rm -f out current.out
-./durandal -i ./very_few_pdbs -d 2.0 --brute -o out -m -1 > /dev/null
+./durandal.cluster_pdbs -i ./very_few_pdbs -d 2.0 --brute -o out -m -1 > \
+/dev/null
 egrep "^members|^pole" out > current.out
 diff reference.out current.out
-./durandal -i ./very_few_pdbs -d 2.0 --smart -o out -m -1 > /dev/null
+./durandal.cluster_pdbs -i ./very_few_pdbs -d 2.0 --smart -o out -m -1 > \
+/dev/null
 egrep "^members|^pole" out > current.out
 diff reference.out current.out
 # test clusters' energy characteristics
-./durandal -i ./very_few_pdbs_e -d 2.0 -s -o out -m -1 -v
+./durandal.cluster_pdbs -i ./very_few_pdbs_e -d 2.0 -s -o out -m -1 -v
 diff out cluster_energies_reference.out
-# test ranker
+# test durandal.rank_pdbs
 rm -f very_few_pdbs_rmsd_current
-./ranker ./very_few_pdbs > very_few_pdbs_rmsd_current
+./durandal.rank_pdbs ./very_few_pdbs > very_few_pdbs_rmsd_current
 diff very_few_pdbs_rmsd_current very_few_pdbs_rmsd_reference
